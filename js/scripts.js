0c9537f91a6a965c54e083532b614246aebc9e53
Zoe Zhou <zoezo85@gmail.com>
2018-06-15 21:39:34 -0700

    it will add bootstrap input fields in html, and set up javascript basic structure

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $("form#contact").submit(function(event) {
+    event.preventDefault();
+
+    var list = [];
+
+    console.log(#produce).val();
+
+    list.push($(#produce).val());
+
+  };
+};
