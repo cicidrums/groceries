79db26f4eb33a5e86be5becc1e6d92995df9eb00
cici harrison and zoe zhou <cici@>
2018-06-18 20:36:57 -0700

    it will dry code by replacing forEach with a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   $("form#grocery").submit(function(event) {
     event.preventDefault();
 
+    //debugger;
+
     var labels = ["produce", "dairy", "protein", "bakery", "alcohol"];
     var groceries = [];
 
@@ -13,16 +15,21 @@ $(document).ready(function() {
 
     var sortedGroceries = groceries.slice();
     sortedGroceries.sort();
+    //var sortedGroceries = groceries.map(function() {
+    //  return groceries.sort();
+    //});
 
     //console.log(groceries);
     //console.log(sortedGroceries);
 
-    var listNumbers = [0, 1, 2, 3, 4];
-
-    listNumbers.forEach(function(listNumber) {
-      $("." + listNumber).text(sortedGroceries[listNumber]);
+    for (var index = 0; index <= sortedGroceries.length; index += 1) {
+      $("." + index).text(sortedGroceries[index]);
+    };
+    //var listNumbers = [0, 1, 2, 3, 4];
+    //listNumbers.forEach(function(listNumber) {
+    //  $("." + listNumber).text(sortedGroceries[listNumber]);
+    //});
 
     $("ul.sorted").show();
-    });
   });
 });
